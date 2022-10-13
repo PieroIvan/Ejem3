fd5af382c95446b6fdf56f0a39772ec9faaf7173
Carlos Palomares <chicho574@gmail.com>
2022-10-12 20:08:22 -0500

    Cambios_y

--- Ejem3/Program.cs
+++ Ejem3/Program.cs
@@ -8,14 +8,14 @@ namespace Ejemplo_15
     {
         static void Main(string[] args)
         {
-            double x, h, a;
+            double y, h, a;
             Console.WriteLine("Ingresa base");
             // ENTRADA
-            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Ingresa altura");
             h = Convert.ToDouble(Console.ReadLine());
             // PROCESO
-            a = x * h / 2;
+            a = y * h / 2;
             // SALIDA
             Console.WriteLine("El área del triangulo es");
             Console.WriteLine(a);
